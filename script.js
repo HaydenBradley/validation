ccb445ee607c6b4da3ef613f482d1cd110995850
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-04-29 22:57:26 -0500

    initial comit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    var username = document.getElementById('username').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var password = document.getElementById('password').value.trim();
+    var usernameError = document.getElementById('usernameError');
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
+  
+    usernameError.textContent = '';
+    emailError.textContent = '';
+    passwordError.textContent = '';
+  
+    if (username.length < 6) {
+      usernameError.textContent = 'Username must be at least 6 characters long.';
+      return;
+    }
+  
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      emailError.textContent = 'Invalid email format.';
+      return;
+    }
+  
+
+    var passwordPattern = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!passwordPattern.test(password)) {
+      passwordError.textContent = 'Password must be at least 8 characters long and contain at least one capital letter and one number.';
+      return;
+    }
+  
+    alert('Registration successful!');
+  });
+  
